a1e1fc587308a8b221151167b07017dc2dd89b17
Sophie Chen <sophiexin036@gmail.com>
2018-11-30 00:41:11 +0800

    modify delete Binary Search Tree node function

--- hw6.cpp
+++ hw6.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-#include "stdlib.h"
 using namespace std;
 
 class TreeNode {
 	friend class BSTree;
 public:
 	/* Constructor */
-	TreeNode(int value=0, TreeNode *l = 0, TreeNode *r = 0) :data(value), leftchild(l), rightchild(r) {}
+	TreeNode(int value = 0, TreeNode *l = 0, TreeNode *r = 0) :data(value), leftchild(l), rightchild(r){}
 private:
 	int data;
 	TreeNode *leftchild;
@@ -54,63 +53,126 @@ public:
 
 	/* delete Node */
 	void deleteNode(int deleteData) {
-		TreeNode *current = Root;
-		if (!current) throw "Binary Search Tree is Empty! Cannot delete.";
+		TreeNode *find = Root, *parent = 0;
+		if (!find) throw "Binary Search Tree is Empty! Cannot delete.";
 
-		current = searchNode(deleteData);
-		if (!current) return; // not found node
-
-		/* delete leaf node that don't have child (ok)*/
-		else if (current->leftchild == 0 && current->rightchild == 0) {
-			delete current;
+		/* search the deleted Node */
+		bool found = false; // not found
+		int pre_direction = 0; // direction of pre to find; left : 1, right : 2
+		while (find) {
+			if (deleteData == find->data) {
+				found = true;
+				break;
+			}
+			else {
+				parent = find;
+				if (deleteData > find->data) {
+					find = find->rightchild;
+					pre_direction = 2;
+				}
+				else {
+					find = find->leftchild;
+					pre_direction = 1;
+				}
+			}
 		}
-		/* delete nonleaf node that has one son (not)*/
-		else if (current->leftchild == 0 || current->rightchild == 0) {
+
+		int childCount = 0;
+		if (find->leftchild != 0) childCount++;
+		if (find->rightchild != 0) childCount++;
+
+		if (found == false) return; // not found node
+
+		/* decide how to delete node */
+		// delete leaf node (including only root in BST) 
+		if (childCount == 0) {
+			/* root case */
+			if (find == Root) Root = 0;
+			/* not root case */
+			else {
+				if (pre_direction == 1) parent->leftchild = 0;
+				else if (pre_direction == 2) parent->rightchild = 0;
+			}
+			delete find;
+		}
+		// delete nonleaf node that has one son 
+		else if (childCount == 1) {
 			/* the tree has a left child */
-			if (current->rightchild == 0) {
-				TreeNode *temp = current->leftchild;
-				current->data = temp->data; // move data to current node
-				delete temp;
+			if (find->rightchild == 0) {
+				/* root case */
+				if (find == Root) {
+					TreeNode *temp = Root;
+					TreeNode *right = find->rightchild;
+					TreeNode *left = find->leftchild;
+					if (temp->leftchild == 0) temp->rightchild = right;
+					else temp->leftchild = left;
+				}
+				/* not root case */
+				else {
+					TreeNode *temp = find->leftchild;
+					if (pre_direction == 1) parent->leftchild = temp;
+					else if (pre_direction == 2) parent->rightchild = temp;
+				}
+				delete find;
 			}
 			/* the tree has a right child */
 			else {
-				TreeNode *temp = current->rightchild;
-				current->data = temp->data; // move data to current node
-				delete temp;
+				/* root case */
+				if (find == Root) {
+					TreeNode *right = find->rightchild;
+					TreeNode *left = find->leftchild;
+					if (find->leftchild == 0) Root = right;
+					else Root = left;
+				}
+				/* not root case */
+				else {
+					TreeNode *temp = find->rightchild;
+					if (pre_direction == 1) parent->leftchild = temp;
+					else if (pre_direction == 2) parent->rightchild = temp;
+				}
+				delete find;
 			}
 		}
 		/* delete nonleaf node that has two son */
-		else if (current->leftchild != 0 && current->rightchild != 0) {
-			TreeNode *link = current->rightchild;
-			TreeNode *temp = current;
-			current = current->leftchild;
-			current->rightchild = link;
-			delete temp;
-		}
-	}
+		else if (childCount == 2) {
+			TreeNode *right = find->rightchild;
+			TreeNode *left = find->leftchild;
+			TreeNode *successor = find->rightchild;
+			TreeNode *parent = find;
 
-	/* Search Node in Binary Search Tree and return address of the node */
-	TreeNode* searchNode(int searchData) {
-		TreeNode *current = Root;
-		if (!current) return 0; // no data, BST is Empty!
-		while (current) {
-			if (searchData > Root->data)	current = current->rightchild;
-			else if (searchData < Root->data) current = current->leftchild;
-			/* delete when encountering same data of Node */
-			else return current;
+			/* delete root */
+			if (find == Root) {
+				while (successor->leftchild) {
+					parent = successor;
+					successor = successor->leftchild;
+				}
+				Root->data = successor->data;
+				parent->rightchild = successor->rightchild;
+				delete successor;
+			}
+			/* delete not root */
+			else {
+				while (successor->leftchild) {
+					parent = successor;
+					successor = successor->leftchild;
+				}
+				find->data = successor->data;
+				if (successor->rightchild != 0) {
+					parent->leftchild = successor->rightchild;
+				}
+				delete successor;
+				parent->rightchild = 0;
+			}
 		}
-		return 0; // not found, so return nullptr
 	}
 
 	/* print Binary Search Tree (Undone) */
 	void printBST() {
 		TreeNode *current = Root;
-		TreeNode *temp = 0;
-
-		cout << "node: ";
+		cout << "node:  ";
 		Inorder1(current);
 		cout << endl;
-		cout << "left: ";
+		cout << "left:  ";
 		Inorder2(current);
 		cout << endl;
 		cout << "right: ";
@@ -121,7 +183,7 @@ public:
 	void Inorder1(TreeNode *current) {
 		if (current) {
 			Inorder1(current->leftchild);
-			cout << current->data << " "; // print node
+			printf("%3d ", current->data); // print node
 			Inorder1(current->rightchild);
 		}
 	}
@@ -129,7 +191,7 @@ public:
 	void Inorder2(TreeNode *current) {
 		if (current) {
 			Inorder2(current->leftchild);
-			cout << ((current->leftchild != NULL) ? current->leftchild->data : 0) << " "; // print leftchild
+			printf("%3d ", ((current->leftchild != NULL) ? current->leftchild->data : 0)); // print leftchild
 			Inorder2(current->rightchild);
 		}
 	}
@@ -137,7 +199,7 @@ public:
 	void Inorder3(TreeNode *current) {
 		if (current) {
 			Inorder3(current->leftchild);
-			cout << ((current->rightchild != NULL)? current->rightchild->data : 0) << " "; // print rightchild
+			printf("%3d ", ((current->rightchild != NULL) ? current->rightchild->data : 0)); // print rightchild
 			Inorder3(current->rightchild);
 		}
 	}
@@ -154,6 +216,5 @@ int main() {
 		inputBST.insertNode(data);
 	}
 	inputBST.printBST();
-	system("pause");
 	return 0;
 }
